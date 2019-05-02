e11dacf2393f84e105d82db1923669d224699acd
manyasundar <manyasundar@gmail.com>
2019-05-02 22:59:16 +0530

    skeleton code

--- c_program.c
+++ c_program.c
@@ -1,4 +1,33 @@
+#include<stdio.h>
+
+/* Dubbu's main function :-) */
 int main()
 {
-	return 1;
+	/*Declaring a varibale which will store the word. */
+	 char input_data[100];
+	
+	/*Asking the user for the word as input and then storing
+	  it in the varible input data declared earlier. */	
+	 printf("Enter a word : ");
+	 scanf("%s",input_data);
+	
+	
+	/*Now similarly ask the user for a letter below.
+	  keep in mind you will have to declare a new variable*/	
+	/* WRITE CODE HERE */
+	
+	/*Now lets write the main algorithm, 
+	  we need to find out how many times that letter occured in that word. */
+	  
+	/*HINT : iterate through the entire word using for loop
+	  and then compare each time if the letter matches , then increment counter 
+	  if it matches ,you'll have to take a new variable for the counter*/
+	/* WRITE CODE HERE */
+
+	
+	/*finally print the counter*/ 
+	/* WRITE CODE HERE */ 
+	
+	
+	return 0;
 }
