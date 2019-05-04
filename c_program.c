64ae4d4251d67e4cd23a0b1b8c9e7ce524a7b7fa
shivaniatmande <atmande_shivani@yahoo.com>
2019-05-04 20:19:46 +0530

    updated code

--- c_program.c
+++ c_program.c
@@ -5,16 +5,21 @@ int main()
 {
 	/*Declaring a varibale which will store the word. */
 	 char input_data[100];
+	 char input_character = 'a';
+	 int counter = 0;
+	 
+	/*Now similarly ask the user for a letter below.
+	  keep in mind you will have to declare a new variable*/	
+	 printf("Enter a character you need to search for : ");
+	 scanf("%c",&input_character);
+	 printf("The character you entered is %c\n",input_character);
 	
 	/*Asking the user for the word as input and then storing
 	  it in the varible input data declared earlier. */	
 	 printf("Enter a word : ");
 	 scanf("%s",input_data);
-	
-	
-	/*Now similarly ask the user for a letter below.
-	  keep in mind you will have to declare a new variable*/	
-	/* WRITE CODE HERE */
+	 printf("The word you entered is %s \n",&input_data);
+
 	
 	/*Now lets write the main algorithm, 
 	  we need to find out how many times that letter occured in that word. */
@@ -22,11 +27,16 @@ int main()
 	/*HINT : iterate through the entire word using for loop
 	  and then compare each time if the letter matches , then increment counter 
 	  if it matches ,you'll have to take a new variable for the counter*/
-	/* WRITE CODE HERE */
-
+	for(int i = 0;input_data[i] != '\0';i++)
+	{
+		if(input_data[i] == input_character)
+		{
+			counter++;
+		}
+	}
 	
 	/*finally print the counter*/ 
-	/* WRITE CODE HERE */ 
+	printf("Number of times the letter occures %d \n",counter); 
 	
 	
 	return 0;
